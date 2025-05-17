5739d2beed00ad560b85ab13dcfe4e8faa41306f
Alamin4465 <alaminhosen2779@gmail.com>
2025-05-17 23:09:47 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,29 @@
 // Firebase কনফিগারেশন
-  const firebaseConfig = {
-    apiKey: "AIzaSyBd-XtWebM0_CTQMdzPK-o-W7FUA05bLH8",
-    authDomain: "income-expenses-c943b.firebaseapp.com",
-    projectId: "income-expenses-c943b",
-    storageBucket: "income-expenses-c943b.firebasestorage.app",
-    messagingSenderId: "628898657958",
-    appId: "1:628898657958:web:2050bcaca828be73c8a667"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBd-XtWebM0_CTQMdzPK-o-W7FUA05bLH8",
+  authDomain: "income-expenses-c943b.firebaseapp.com",
+  projectId: "income-expenses-c943b",
+  storageBucket: "income-expenses-c943b.appspot.com",
+  messagingSenderId: "628898657958",
+  appId: "1:628898657958:web:2050bcaca828be73c8a667"
+};
 
-  // Firebase ইনিশিয়ালাইজেশন
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase .firestore();
+// Firebase ইনিশিয়ালাইজ করুন
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// রিয়েলটাইম ডেটা ট্র্যাকিং
+let transactions = [];
+let currentUser = null;
+
+// ইউজার লগইন স্টেট ট্র্যাক করুন
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    currentUser = user;
+    loadTransactions();
+    updateWelcomeMessage(user.email);
+  } else {
+    window.location.href = "login.html"; // লগইন পেজে রিডাইরেক্ট
+  }
+});
